f31760f7b63e64fe99a2c500f8f1831f60f0542d
zuxin <seanzuxin@outlook.com>
2018-08-30 23:33:23 +0800

    修改了request以及并入了主程序

--- getNVR.py
+++ getNVR.py
@@ -1,28 +1,42 @@
 import cv2
+import time
 import requests
-from PIL import Image
 
-
-# 摄像头列表
-# 正常是26-43 是学校的摄像头不过有一些不能用
+# 图像传入地址
+url = r"http://127.0.0.1:8888/upload_to_tegu"
+# 上传用缓存文件
+files = {'file': (r'cache.png', open('cache.png', 'rb'), 'image/png', {})}
+# 摄像头列表 正常是26-43 是学校的摄像头可能有一些不能用
 cameraList = [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43]
 # 每次取视频的循环间隔时间
 sleepTime = 10
-# 是否开始检测
-isStarted = False
 # 摄像头信号源列表
 captures = []
 
+
 # 定义视频信号源
-for i in cameraList:
-    vc = cv2.VideoCapture('rtsp://admin:Ccut@edu2017@10.205.12.' + str(i) + ':554/Streaming/Channels/101?transportmode=unicast')
-    # print(vc.isOpened())
-    if vc.isOpened():
-        captures.append(vc)
-    else:
-        del cameraList[i]
+def define_captures():
+    for i in cameraList:
+        cap = cv2.VideoCapture('rtsp://admin:Ccut@edu2017@10.205.12.' + str(i) + ':554/Streaming/Channels/101?transportmode=unicast')
+        # print(vc.isOpened())
+        # 检测摄像头是否能用并添加进列表
+        if cap.isOpened():
+            captures.append(cap)
+        else:
+            del cameraList[i]
 
 
 # 将摄像头信号送入后端
-def input_video_capture():
-    pass
+def input_video_capture(isStarted):
+    while isStarted:
+        for j in captures:
+            try:
+                rval, frame = j.read()
+                cv2.imwrite('cache.png', frame)
+                cv2.waitKey(1)
+                r = requests.post(url, files=files)
+                print(r.text)
+            except:
+                print("第" + str(captures.index(j)) + "个摄像头发生错误")
+        time.sleep(sleepTime)
+
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
 import requests
 import time
+import pygame
+import json
+import logging
+import getNVR
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QDialog
 from firstMainWin import Ui_mainWindow
 from PyQt5.QtCore import QTimer, QDateTime, pyqtSignal, QThread
 
 
-url = "http://bbs.ngacn.cc"   # 测试用地址
+url = r"http://127.0.0.1:8888/get_"   # 返回地址
 humanWarning = ""  # 出现人的警告信息
 hydrantWarning = ""  # 消防设备警告信息
+# 播放警报音
+file = 'warning.mp3'
+pygame.mixer.init()
+track = pygame.mixer.music.load(file)
 
 
 class MyMainWindow(QMainWindow, Ui_mainWindow):
@@ -16,75 +24,102 @@ class MyMainWindow(QMainWindow, Ui_mainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
-        '''
-        # 子窗口
-        self.mdi = QMdiArea()
-        self.setCentralWidget(self.mdi)
-        '''
         # 设置定时器将定时器的信号与槽链接
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.show_time)
         # 设置时间间隔并启动
         self.timer.start(1000)
+        self.init_caps()
         self.init_ui()
+
+    def init_caps(self):
+        self.sendmessage = SendCaptureThread()
+        self.sendmessage.start()
 
     def init_ui(self):
         # 创建线程
         self.backend = BackendThread()
         self.backend.warning.connect(self.show_message)
         self.backend.start()
-    # 显示时间
 
+    # 显示时间
     def show_time(self):
         time = QDateTime.currentDateTime()
         time_display = time.toString("yyyy-MM-dd hh:mm:ss dddd")
         self.label_3.setText(time_display)
-    # 警告信息
 
+    # 警告信息
     def show_message(self, data):
         # print(data)
-        if data == "摄像头监测到有人出没" or "摄像头检测到消防设备被移动":
+        if data == "human":
+            self.label.setText("监测到"+info[0][0]+"的摄像头出现人")
             self.setStyleSheet('''
             #label { color: red }
                                     ''')
-        elif data == "网络错误请检查网络":
+            pygame.mixer.music.play()
+            time.sleep(10)
+            pygame.mixer.music.stop()
+        # if data == "hydrant":
+        #   self.label.setText("监测到"+info[0][0]+"的摄像头出现设备移动")
+        #   self.setStyleSheet('''
+        #   #label { color: red }
+        #                           ''')
+        #   pygame.mixer.music.play()
+        #   time.sleep(10)
+        #   pygame.mixer.music.stop()
+        elif data == "error":
+            self.label.setText("网络出现错误")
             self.setStyleSheet('''
             #label { color: orange }
                                     ''')
-        elif data == "正常":
+        elif data == "normal":
+            self.label.setText("正常")
             self.setStyleSheet('''
                 #label { color: green; 
                 }
                 ''')
-        self.label.setText(data)
+
 
 
 class BackendThread(QThread):
     # 通过类成员定义信号
     warning = pyqtSignal(str)
-    # 处理逻辑
 
+    # 将返回的信号进行检验
     def run(self):
         while True:
             try:
                 r = requests.get(url)
                 r.encoding = "utf-8"
                 state = r.status_code
-            except:
-                state = "404"
+                global info
+                info = json.load(r.content)
+            except (ValueError, ConnectionError, requests.HTTPError, requests.Timeout, requests.TooManyRedirects) as e:
+                state = 404
+                logging.exception(e)
             if state == 200:
-                # print("连接成功"+str(state))
-                # 暂定
-                if r.text == humanWarning:
-                    self.warning.emit("摄像头监测到有人出没")
-                elif r.text == hydrantWarning:
-                    self.warning.emit("摄像头检测到消防设备被移动")
+                print("连接成功"+str(state))
+                if info[0][1] == "human":
+                    self.warning.emit("human")
+                elif info[0][1] == "hydrant":
+                    self.warning.emit("hydrant")
                 else:
-                    self.warning.emit("正常")
+                    self.warning.emit("normal")
             else:
-                # print("连接失败"+str(state))
-                self.warning.emit("网络错误请检查网络")
+                print("连接失败"+str(state))
+                self.warning.emit("error")
             time.sleep(1)
+
+
+class SendCaptureThread(QThread):
+    # 获取视频信息
+    def run(self):
+        getNVR.define_captures()
+        if getNVR.captures:
+            isStarted = True
+        else:
+            isStarted = False
+        getNVR.input_video_capture(isStarted)
 
 
 if __name__ == "__main__":
