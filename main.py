400878c701743c87ef22d40761a53663b2de8c1c
zuxin <seanzuxin@outlook.com>
2018-09-04 23:36:00 +0800

    改善页面逻辑

--- main.py
+++ main.py
@@ -18,14 +18,6 @@ sleepTime = 10  # 每次取视频的循环间隔时间
 file = "warning.mp3"  # 警报音
 
 
-# 播放警告音乐
-def play_warning_music():
-    pygame.mixer.music.load(file)
-    pygame.mixer.music.play()
-    time.sleep(10)
-    pygame.mixer.music.stop()
-
-
 class MyMainWindow(QMainWindow, Ui_mainWindow):
 
     def __init__(self, parent=None):
@@ -36,13 +28,15 @@ class MyMainWindow(QMainWindow, Ui_mainWindow):
         self.timer.timeout.connect(self.show_time)
         # 设置时间间隔并启动
         self.timer.start(1000)
-        self.init_messages()
+        self.init_UI()
 
-    def init_messages(self):
-        # 创建线程并连接信号槽
-        self.getmessage = BackendThread()
-        self.getmessage.warning.connect(self.show_message)
-        self.getmessage.start()
+    def init_UI(self):
+        # 创建线程
+        self.backend = BackendThread()
+        # 连接信号
+        self.backend.warning.connect(self.show_message)
+        # 开始线程
+        self.backend.start()
 
     # 显示时间
     def show_time(self):
@@ -51,89 +45,123 @@ class MyMainWindow(QMainWindow, Ui_mainWindow):
         self.label_3.setText(time_display)
 
     # 警告信息
-    def show_message(self, data):
-        # print(data)
-        if data.startswith("1"):
-            self.label.setText(data)
-            self.setStyleSheet('''
-            #label { color: red }
-                                    ''')
-            play_warning_music()
-        elif data.startswith("2"):
-            self.label.setText(data)
-            self.setStyleSheet('''
-            #label { color: red }
-                                     ''')
-            play_warning_music()
-        elif data.startswith("3") or data.startswith("4"):
-            self.label.setText(data)
-            self.setStyleSheet('''
-            #label { color: orange }
-                                    ''')
-        elif data == "正常":
-            self.label.setText(data)
+    def show_message(self, message):
+        print(message)
+        if message == "正常":
+            self.label.setText(message)
             self.setStyleSheet('''
                 #label { color: green; 
                 }
                 ''')
+        elif "警告" in message:
+            self.label.setText(message)
+            self.setStyleSheet('''
+            #label { color: red }
+                                    ''')
+            # 播放警告音乐
+            pygame.mixer.music.play()
+        else:
+            self.label.setText(message)
+            self.setStyleSheet('''
+            #label { color: orange }
+                                    ''')
 
 
 # 子线程处理后台逻辑防止堵塞
 class BackendThread(QThread):
     # 通过类成员定义信号
     warning = pyqtSignal(str)
-
+    '''
+    # 测试用代码
+    def run(self):
+        # 测试是否能连接tegu
+        try:
+            re = requests.get(url)
+            re.encoding = "utf-8"
+            global state
+            state = re.status_code
+            # print(state)
+        except (ValueError, ConnectionError, requests.HTTPError, requests.Timeout, requests.TooManyRedirects) as e:
+            state = 404
+            logging.exception(e)
+            self.warning.emit("网络连接错误")
+        while True:
+            if state == 200:
+                # 将每个摄像头信息传入tegu进行监测
+                global cam_id, warning_cam
+                warning_cam = ""
+                for cam_id in range(10):
+                    # print(cam_id)
+                    # 用post方法将缓存图片传入tegu
+                    r = requests.post(r"http://127.0.0.1:8888/upload_to_tegu")
+                    # print(r.status_code)
+                    # 获得json文件然后读取
+                    info = json.loads(r.content)
+                    # print(info)
+                    # 判断返回情况只有晚上才开始判断,将返回的嵌套列表flatten然后查看是否含有元素
+                    if humanWarning in list(chain.from_iterable(info)) and (
+                            int(time.strftime("%H")) >= 22 or int(time.strftime("%H")) <= 7):
+                        warning_cam = warning_cam + "警告，摄像头"+str(cam_id)+"发现有人出没\n"
+                    elif hydrantWarning in list(chain.from_iterable(info)):
+                        warning_cam = warning_cam + "警告，摄像头" + str(cam_id) + "发现消防器材被移动\n"
+                if warning_cam is not None:
+                    self.warning.emit(warning_cam)
+                else:
+                    self.warning.emit("正常")
+            else:
+                self.warning.emit("网络连接错误")
+                # SleepTime秒后进行监测
+            time.sleep(sleepTime)
+    '''
     # 将返回的信号进行检验
     def run(self):
         # 定义视频信号源
         getNVR.define_captures()
         while True:
-            # 监测摄像头连接是否为空如果为空则大概率为网络问题
-            if getNVR.captures:
-                # 测试是否能连接tegu
-                try:
-                    r = requests.get(url)
-                    r.encoding = "utf-8"
-                    global state
-                    state = r.status_code
-                except (ValueError,
-                        ConnectionError, requests.HTTPError, requests.Timeout, requests.TooManyRedirects) as e:
-                    state = 404
-                    logging.exception(e)
-                    self.warning.emit("4网络连接错误")
+            # 将每个摄像头信息传入tegu进行监测
+            global cam_id, warning_message
+            warning_message = ""
+            for cam_id, cam_capture in getNVR.captures:
+                # print(cam_id)
+                # 判断摄像头是否正常工作
+                if cam_capture is not None:
+                    # 生成缓存文件以监测
+                    getNVR.make_cache(cam_capture)
+                    try:
+                        # 用post方法将缓存图片传入tegu
+                        global r
+                        r = requests.post(r"http://127.0.0.1:8888/upload_to_tegu", files={'file': (r'cache.png', open(
+                                    'cache.png', 'rb'), 'image/png', {})})
+                        r.encoding = "utf-8"
+                        # 获得json文件然后读取
+                        global info
+                        info = json.loads(r.content)
+                        # 判断返回情况并且只有晚上才开始判断,将返回的嵌套列表flatten然后查看是否含有元素
+                        if humanWarning in list(chain.from_iterable(info)) and (
+                                int(time.strftime("%H")) >= 22 or int(time.strftime("%H")) <= 7):
+                            warning_message = warning_message + "警告，摄像头" + str(cam_id) + "发现有人出没\n"
+                        elif hydrantWarning in list(chain.from_iterable(info)):
+                            warning_message = warning_message + "警告，摄像头" + str(cam_id) + "发现消防器材被移动\n"
+                    except(ValueError,
+                           ConnectionError, requests.HTTPError, requests.Timeout, requests.TooManyRedirects) as e:
+                        # 错误处理
+                        logging.exception(e)
+                        warning_message = warning_message + "摄像头" + str(cam_id) + "连接错误\n"
+                        # print(r.status_code)
                 else:
-                    state = 404
-                    self.warning.emit("4网络连接错误")
-                if state == 200:
-                    # 将每个摄像头信息传入tegu进行监测
-                    for cam_id, cam_capture in getNVR.captures:
-                        global cam_id
-                        # 判断摄像头是否正常工作
-                        if cam_capture is not None:
-                            # 生成缓存文件以监测
-                            getNVR.make_cache(cam_capture)
-                            # 用post方法将缓存图片传入tegu
-                            r = requests.post(url, files=files)
-                            # 获得json文件然后读取
-                            info = json.loads(r.content)
-                            # 判断返回情况只有晚上才开始判断,将返回的嵌套列表flatten然后查看是否含有元素
-                            if humanWarning in list(chain.from_iterable(info)) and (
-                                    int(time.strftime("%H")) > 22 or int(time.strftime("%H")) < 7):
-                                self.warning.emit("1警告，监测到" + cam_id + "号摄像头有人出没")
-                            elif hydrantWarning in list(chain.from_iterable(info)):
-                                self.warning.emit("2警告，监测到" + cam_id + "号摄像头有消防设备移动")
-                            else:
-                                self.warning.emit("正常")
-                else:
-                    self.warning.emit("3" + cam_id + "号摄像头连接失败" + str(state))
-                # SleepTime秒后进行监测
-                time.sleep(sleepTime)
+                    # 空摄像头处理
+                    warning_message = warning_message + "摄像头" + str(cam_id) + "无法获取\n"
+            if warning_message is not None:
+                self.warning.emit(warning_message)
             else:
-                self.warning.emit("4网络连接错误")
+                self.warning.emit("正常")
+            # sleepTime秒后再次执行
+            time.sleep(sleepTime)
 
 
 if __name__ == "__main__":
     pygame.mixer.init()
+    pygame.mixer.music.load(file)
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
     qssStyle = '''
