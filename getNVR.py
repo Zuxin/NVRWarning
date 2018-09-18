eace63ffd51c5a6f9e6bb4a9921b73202f89365f
zuxin <seanzuxin@outlook.com>
2018-09-18 20:27:41 +0800

    Fix Bugs and add ip info

--- getNVR.py
+++ getNVR.py
@@ -1,39 +1,57 @@
+# pylint: disable=no-member
 import cv2
 
 
 # 摄像头列表 正常是26-43 是学校的摄像头可能有一些不能用
-cameraList = [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43]
+cameraList = [
+    "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37",
+    "39", "40", "41", "42", "43"
+]
+# 摄像头对应的地点
+cap_location = [
+    "老图书馆3F-1", "老图书馆3F-2", "老图书馆3F-3", "老图书馆3F-4", "老图书馆3F-5", "老图书馆3F-6",
+    "老图书馆3F-7", "老图书馆3F-8", "老图书馆3F-9", "老图书馆3F-9", "老图书馆3F-10", "老图书馆3F-11",
+    "老图书馆4F-1", "老图书馆4F-3", "老图书馆4F-4", "老图书馆4F-5", "老图书馆4F-6", "老图书馆4F-7"
+]
 # 摄像头信号源列表
 captureList = []
-captures = {}
+# captures = {}
 
 
 # 定义视频信号源
 def define_captures():
     for i in cameraList:
         # 经过RTSP协议获得摄像数据
-        cap = cv2.VideoCapture('rtsp://admin:Ccut@edu2017@10.205.12.' + str(i) +
-                               ':554/Streaming/Channels/101?transportmode=unicast')
-        # print(cap.isOpened())
+        # print("第"+str(i)+"号摄像头检测")
+        try:
+            cap = cv2.VideoCapture(
+                'rtsp://admin:Ccut@edu2017@10.205.12.' + i +
+                ':554/Streaming/Channels/101?transportmode=unicast')
+        except Exception:
+            print(str(i) + "号摄像头存在问题")
         # 检测摄像头是否能用并添加进列表
+        print(cap.isOpened())
         if cap.isOpened():
             captureList.append(cap)
         else:
             captureList.append(None)
-        print(cap)
-    global captures
-    captures = dict(zip(cameraList, captureList))
 
 
 # 读取并生成cache文件
+
+
 def make_cache(cam_capture):
-    if not isinstance(cam_capture, cv2.VideoCapture):
+    if not isinstance(cam_capture, (cv2.VideoCapture, None)):
         #  参数类型检查
         raise TypeError('bad operand type')
     rval, frame = cam_capture.read()
     cv2.imwrite('cache.png', frame)
-    if not rval:
+    # cv2.imread(frame)
+    '''
+    if rval:
         #  检查是否正常输出
-        raise TypeError('bad operand type')
-    cv2.waitKey(1)
-
+        pass
+    '''
+    return rval
+    cv2.release(cam_capture)
+    cv2.waitKey(10)
--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from firstMainWin import Ui_mainWindow
 from PyQt5.QtCore import QTimer, QDateTime, pyqtSignal, QThread
 
-url = r"http://127.0.0.1:8888/upload_to_tegu"  # 连接tegu
+tegu_ip = "127.0.0.1"
+url = r"http://" + tegu_ip + ":8888/upload_to_tegu"  # 连接tegu
 humanWarning = "person"  # 出现人的警告信息
 hydrantWarning = "hydrant"  # 消防设备警告信息
 sleepTime = 10  # 每次取视频的循环间隔时间
@@ -18,17 +19,19 @@ warning_sound = "warning.mp3"  # 警报音
 
 
 def console_out(log_filename):
-    # Define a Handler and set a format which output to file
+    # 定义一个Handler
     logging.basicConfig(
         level=logging.DEBUG,  # 定义输出到文件的log级别，
-        format='%(asctime)s  %(filename)s : %(levelname)s  %(message)s',  # 定义输出log的格式
+        format='%(asctime)s  %(filename)s : %(levelname)s  %(message)s',
         datefmt='%Y-%m-%d %A %H:%M:%S',  # 时间
         filename=log_filename,  # log文件名
         filemode='a')  # 写入模式“w”或“a”
     # 定义Handler
     console = logging.StreamHandler()  # 定义console handler
     console.setLevel(logging.INFO)  # 定义该handler级别
-    formatter = logging.Formatter('%(asctime)s  %(filename)s : %(levelname)s  %(message)s')  # 定义该handler格式
+    formatter = logging.Formatter(
+        '%(asctime)s  %(filename)s : %(levelname)s  %(message)s'
+    )  # 定义该handler格式
     console.setFormatter(formatter)
     logging.getLogger().addHandler(console)  # 实例化添加handler
 
@@ -48,6 +51,8 @@ class MyMainWindow(QMainWindow, Ui_mainWindow):
         self.timer.timeout.connect(self.show_time)
         # 设置时间间隔并启动
         self.timer.start(1000)
+        # 设置显示当前ip
+        self.show_ip()
         self.init_ui()
 
     def init_ui(self):
@@ -58,19 +63,23 @@ class MyMainWindow(QMainWindow, Ui_mainWindow):
         # 开始线程
         self.backend.start()
 
+    # 显示当前ip地址
+    def show_ip(self):
+        self.label_4.setText(tegu_ip)
+
     # 显示时间
     def show_time(self):
         time = QDateTime.currentDateTime()
         time_display = time.toString("yyyy-MM-dd hh:mm:ss dddd")
-        self.label_3.setText(time_display)
+        self.label_3.setText("当前ip" + time_display)
 
     # 警告信息
     def show_message(self, message):
         print(message)
         if message == "正常":
-            self.label.setText(message)
+            self.label.setText("正常")
             self.setStyleSheet('''
-                #label { color: green; 
+                #label { color: green;
                 }
                 ''')
         elif "警告" in message:
@@ -99,14 +108,14 @@ class BackendThread(QThread):
             getNVR.define_captures()
         except Exception as e:
             logging.warning(e)
-            raise e
+            # 判断是否为空
+
         while True:
             try:
                 # 将每个摄像头信息传入tegu进行监测
-                global cam_id, warning_message
+                global warning_message
                 warning_message = ""
-                for cam_id, cam_capture in getNVR.captures:
-                    print(cam_id, cam_capture)
+                for cam_capture in getNVR.captureList:
                     # 判断摄像头是否正常工作
                     if cam_capture is not None:
                         # 生成缓存文件以监测
@@ -114,33 +123,49 @@ class BackendThread(QThread):
                         try:
                             # 用post方法将缓存图片传入tegu
                             global r
-                            r = requests.post(r"http://127.0.0.1:8888/upload_to_tegu",
-                                              files={'file': (r'cache.png',  open('cache.png', 'rb'), 'image/png', {})})
+                            r = requests.post(
+                                r"http://127.0.0.1:8888/upload_to_tegu",
+                                files={
+                                    'file': (r'cache.png',
+                                             open('cache.png', 'rb'),
+                                             'image/png', {})
+                                })
                             r.encoding = "utf-8"
                             # 获得json文件然后读取
                             global info
                             info = json.loads(r.content)
+                            cam_id = getNVR.captureList.index(cam_capture)
+                            # print(info)
                             # 判断返回情况并且只有晚上才开始判断,将返回的嵌套列表flatten然后查看是否含有元素
-                            if humanWarning in list(chain.from_iterable(info)) and (
-                                    int(time.strftime("%H")) >= 22 or int(time.strftime("%H")) <= 7):
-                                warning_message = warning_message + "警告，摄像头" + str(cam_id) + "发现有人出没\n"
-                            elif hydrantWarning in list(chain.from_iterable(info)):
-                                warning_message = warning_message + "警告，摄像头" + str(cam_id) + "发现消防器材被移动\n"
-                        except(ValueError, ConnectionError,
-                               FileNotFoundError, requests.HTTPError, requests.Timeout, requests.TooManyRedirects) as e:
+                            if humanWarning in list(
+                                    chain.from_iterable(info)) and (
+                                        int(time.strftime("%H")) >= 22
+                                        or int(time.strftime("%H")) <= 7):
+                                warning_message = (
+                                    warning_message + "警告，摄像头"+str(
+                                        getNVR.cap_location[cam_id]) +
+                                    "发现有人出没\n")
+                            elif hydrantWarning in list(
+                                    chain.from_iterable(info)):
+                                print(warning_message)
+                        except (ValueError, ConnectionError, FileNotFoundError,
+                                requests.HTTPError, requests.Timeout,
+                                requests.TooManyRedirects) as e:
                             # 错误处理
                             logging.error(e)
                             logging.exception(e)
-                            warning_message = warning_message + "摄像头" + str(cam_id) + "连接错误\n"
+                            warning_message = (
+                                warning_message + "摄像头" + "连接错误\n")
                             print(r.status_code)
                     else:
                         # 空摄像头处理
                         print("错误")
-                        warning_message = warning_message + "摄像头" + str(cam_id) + "无法获取\n"
-                if warning_message is not None:
-                    self.warning.emit(warning_message)
-                else:
-                    self.warning.emit("正常")
+                        warning_message = warning_message + "摄像头" + "无法获取\n"
+                    print(warning_message)
+                    if warning_message is not None:
+                        self.warning.emit(warning_message)
+                    else:
+                        self.warning.emit("正常")
                 # sleepTime秒后再次执行
                 time.sleep(sleepTime)
             except Exception as e:
@@ -158,7 +183,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
     qssStyle = '''
-    #label { color: orange; 
+    #label { color: orange;
     }
     #label_3 { color: blue
     }
